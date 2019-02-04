ba32a7bd8eca090eca288c3124ebc23f2ea8b114
Tim Urista <timothy.urista@gmail.com>
2019-02-04 08:17:07 -0800

    add example of using hashmap and args

---
+++ enums-demo/target/debug/deps/enums_demo-589a4e9288dd5e4c.d
@@ -0,0 +1,5 @@
+/Users/timurista/projects/rust-examples/enums-demo/target/debug/deps/enums_demo-589a4e9288dd5e4c: src/main.rs
+
+/Users/timurista/projects/rust-examples/enums-demo/target/debug/deps/enums_demo-589a4e9288dd5e4c.d: src/main.rs
+
+src/main.rs:
