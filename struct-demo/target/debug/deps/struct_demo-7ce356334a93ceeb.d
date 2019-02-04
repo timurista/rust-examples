ed9931c48c54349acaef0229748a8f207a163d63
Tim Urista <timothy.urista@gmail.com>
2019-02-04 06:27:29 -0800

    add examples for structs in rust

---
+++ struct-demo/target/debug/deps/struct_demo-7ce356334a93ceeb.d
@@ -0,0 +1,5 @@
+/Users/timurista/projects/rust-examples/struct-demo/target/debug/deps/struct_demo-7ce356334a93ceeb: src/main.rs
+
+/Users/timurista/projects/rust-examples/struct-demo/target/debug/deps/struct_demo-7ce356334a93ceeb.d: src/main.rs
+
+src/main.rs:
